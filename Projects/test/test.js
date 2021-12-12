bb133cafc433c995b4ab4d847792e5967369f2fa
Lets-Code-with-Prince <pahadiprince2008@gmail.com>
2021-12-12 22:47:48 +0530

    adding components to my project

---
+++ Projects/test/test.js
@@ -0,0 +1,14 @@
+const places = ['Tokyo', 'China', 'Delhi', 'New Zealand']
+
+places[0] = 'America'
+console.log(places[0])
+
+// places.pop()
+// console.log(places)
+
+// delete places[2]
+// console.log(places)
+
+places.unshift("Switzerland")
+console.log(places.length);
+console.log(places)
